ba0876130c07695114d87d519b4338b3fae7dcb0
ashwinkaruppanan <ashwinak148@gmail.com>
2023-12-21 17:22:30 +0530

    final

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,13 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({}),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
