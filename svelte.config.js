6006db5a8415ef11ee83f4471b09998789266b4b
ashwinkaruppanan <ashwinak148@gmail.com>
2023-12-21 17:13:35 +0530

    final

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
-
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
-	},
-	paths: {
-		base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		adapter: adapter({}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	},
 	preprocess: vitePreprocess()
+
 
 };
 
